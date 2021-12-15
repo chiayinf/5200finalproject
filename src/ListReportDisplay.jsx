e61b38875157defc8c4fab10e4f2de8362bea947
chiayinf <chiayinfan0917@gmail.com>
2021-12-15 14:38:03 +0800

    search location add detail

--- src/ListReportDisplay.jsx
+++ src/ListReportDisplay.jsx
@@ -9,13 +9,43 @@ import Button from "react-bootstrap/Button";
 
 export default function ListReportDisplay(props) {
   const allReports = props.reports;
+  console.log('the type of allReports is ', typeof allReports);
+  console.log('all reports', allReports.data);
+  console.log('a report', allReports.data[0]);
+  // const reportListComponent = [];
+  /* for (let report of allReports.data){
+    reportListComponent.push(report);
+  }
+  console.log('5 reports', reportListComponent); */
 
-  const jobListComponent = ['year', 'locat'];
+  /* for (let i = 0; i < 5; i++) {
+    reportListComponent.push(allReports.data[i]);
+  }
+  console.log('5 reports', reportListComponent); */
+  const reportListComponent = allReports.data.map((report) => {
+    return (
+      <>
+        <p></p>
+        <ListGroup as="ul">
+          <ListGroup.Item as="li" active>
+          Report Number: {report.reportNumber}
+          </ListGroup.Item>
+          <ListGroup.Item as="li">Address: {report.blockAddress}</ListGroup.Item>
+          <ListGroup.Item as="li">Period: {report.offenseStartDateTime} to {report.offenseEndDateTime}</ListGroup.Item>
+          <ListGroup.Item as="li">Report Time: {report.reportDateTime}</ListGroup.Item>
+          <ListGroup.Item as="li">Crime Type: {report.crimeAgainstCategory}</ListGroup.Item>
+          <ListGroup.Item as="li">Crime Detail: {report.offense}</ListGroup.Item>
+        </ListGroup>
+        
+      </>
+    );
+  });
+
   return (
     <>
       <div class="jobListDisplay">
         <ListGroup as="ol" numbered>
-          {jobListComponent}
+          {reportListComponent}
         </ListGroup>
       </div>
     </>
